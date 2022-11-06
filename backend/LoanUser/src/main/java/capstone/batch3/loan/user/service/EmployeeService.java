6fd59ce27360bd85ec244a1b07cd8693581e9032
Umakanth Sahu <69105946+UmakanthSahu@users.noreply.github.com>
2022-11-06 13:46:27 +0530

    fixed conflicts

--- backend/LoanUser/src/main/java/capstone/batch3/loan/user/service/EmployeeService.java
+++ backend/LoanUser/src/main/java/capstone/batch3/loan/user/service/EmployeeService.java
@@ -16,13 +16,8 @@ public class EmployeeService {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 
-<<<<<<< HEAD
-	public static Employee login(String email, String password) {
-		Employee cus = employeeRepository.findByEmail(email);
-=======
 	public Employee login(String email, String password) {
 		Employee cus = employeeRepository.findByEmailId(email);
->>>>>>> srijo223
 		System.err.println(cus);
 		if (cus == null) {
 			throw new GlobalLoanException("404", "No customer with this credential");
@@ -35,11 +30,7 @@ public class EmployeeService {
 		}
 	}
 
-<<<<<<< HEAD
-	public static Employee addEmployee(Employee employee) {
-=======
 	public Employee addEmployee(Employee employee) {
->>>>>>> srijo223
 		if (existsById(employee)) {
 			return null;
 		}
@@ -49,19 +40,11 @@ public class EmployeeService {
 		return employee;
 	}
 
-<<<<<<< HEAD
-	private static boolean existsById(Employee employee) {
-		return employeeRepository.existsById(employee.getEmailId());
-	}
-
-	private static boolean addItemToLoanCard(Loancard loancard, ItemMaster it) {
-=======
 	private boolean existsById(Employee employee) {
 		return employeeRepository.existsById(employee.getEmailId());
 	}
 
 	private boolean addItemToLoanCard(Loancard loancard, ItemMaster it) {
->>>>>>> srijo223
 		if (it.getIssue_status() == ItemIssueStatus.NOTISSUED) {
 			it.setIssue_status(ItemIssueStatus.ISSUED);
 			loancard.addNewItems(it);
